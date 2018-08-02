650b3330fed70d00f0ecb66276691294a74af820
KalagurAndrey <a.kalagur@robo.finance>
2018-08-02 16:59:09 +0700

    Форма регистрации и скрипт добавления данных в БД

--- create-account.php
+++ create-account.php
@@ -1,4 +1,26 @@
 <?php
-$pdo = new PDO('mysql:host=127.0.0.1; dbname=SocialNetwork; charset=utf8', 'root', '');
+    include ('classes/DB.php');
 
+    if(isset($_POST['createaccount'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)',
+            array(':username' => $username, ':password' => $password, ':email' => $email));
+        echo 'Успешно';
+
+    }
 ?>
+
+
+
+
+<h1>Регистрация:</h1>
+
+<form action="create-account.php" method="post">
+    <input type="text" name="username" placeholder="Имя пользователя"> <p />
+    <input type="password" name="password" placeholder="Пароль"><p />
+    <input type="email" name="email" placeholder="E-mail"><p/>
+    <input type="submit" name="createaccount" value="Зарегистрироваться">
+</form>
+
